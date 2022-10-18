5b5b27c9145c750c05dbcd8859620365a2c604ca
Rahul <tiwarirj73@gmail.com>
2022-10-18 20:28:11 +0530

    home

--- @core/resources/views/frontend/home-pages/home-02.blade.php
+++ @core/resources/views/frontend/home-pages/home-02.blade.php
@@ -126,7 +126,7 @@
 <section class="our-work-area padding-top-110 padding-bottom-120">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <div class="section-title desktop-center margin-bottom-55">
                     <h2 class="title">{{get_static_option('home_page_01_'.$user_select_lang_slug.'_recent_work_title')}}</h2>
                     <p>{{get_static_option('home_page_01_'.$user_select_lang_slug.'_recent_work_description')}}</p>
