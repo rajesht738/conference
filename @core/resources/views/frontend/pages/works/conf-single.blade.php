70a5cc652f1f069296d3883b297e740c012223d6
Rahul <tiwarirj73@gmail.com>
2022-11-10 14:00:39 +0530

    front

--- @core/resources/views/frontend/pages/works/conf-single.blade.php
+++ @core/resources/views/frontend/pages/works/conf-single.blade.php
@@ -237,7 +237,7 @@ color: lightseagreen;">
                 <p class="lead font-weight-light text-400">Once you submit the abstract, we will get in touch with you in 4-5 business days regarding the status (Accepted/Rejected) of the submission. If you do not hear back from us, please contact at: 
                 <a href="mailto:contact@biocoreconferences.com" style="color:#fff;text-transform:lowercase">contact@biocoreconferences.com
                 </a></p>
-                <a class="btn btn-lg btn-danger rounded-capsule mt-4" href="http://conf.micropowersystem.org/conferences/biotech2021/abstract-submission" title="Closed!" data-fancyscroll="data-fancyscroll">Submit now</a>
+                <a class="btn btn-lg btn-danger rounded-capsule mt-4" href="http://conf.micropowersystem.org/conferences/{{$work_item->slug}}/abstract-submission" title="Closed!" data-fancyscroll="data-fancyscroll">Submit now</a>
               </div>
             </div>
         </div>
